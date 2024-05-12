9d694628bf120a17794ac29732e3d9c80b612d57
akshu59 <akshupathania2259@gmail.com>
2024-05-12 13:39:20 +0530

    backend fix

---
+++ Backend/README.md
@@ -0,0 +1,2 @@
+# AR_Store
+ AR store developed using UE 5 as front end and Spring Boot as backend + MySQL DB
---
+++ Backend/src/test/java/com/akshu/springboot2/jpahibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshu.springboot2.jpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
